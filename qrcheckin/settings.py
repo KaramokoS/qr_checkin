af1dc4b345d698d01ef5263c8014760fabd6d373
kara <samss.kara@gmail.com>
2025-12-02 00:18:13 +0100

    Fix Heroku static configuration

--- qrcheckin/settings.py
+++ qrcheckin/settings.py
@@ -65,4 +65,8 @@ CHANNEL_LAYERS = {
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
